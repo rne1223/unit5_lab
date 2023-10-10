43001023defbb74c9365858add544aa8263638b6
rne1223 <rne1223@gmail.com>
2023-10-10 02:04:53 -0700

    done

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+* A small project that displays information about cryptocurrencies, such as their value, market cap, and percent change in value, which you will access using API calls. ![Alt text](public/unit5_lab.gif)
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import CoinInfo from "./components/coinInfo";
 
+function App() {
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const URL = import .meta.env.VITE_URL;
+
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(`${URL}/all/coinlist?api_key=${API_KEY}`);
+      const json = await response.json();
+      setList(json);
+    };
+
+    fetchAllCoinData().catch(console.error);
+  }, [])
+
+
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+
+    if(searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item).join("").toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+
+      <input 
+        type="text" 
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+
+      <ul>
+         {searchInput.length > 0
+      ? filteredResults.map((coin, index) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              key={index}
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+        )
+      : list && Object.entries(list.Data).map(([coin],index) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              key={index}
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+        )}
+      </ul>
+    </div>
+  ); 
 }
 
 export default App
